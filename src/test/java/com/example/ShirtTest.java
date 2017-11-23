58b7d952ead580fd28fbf9efa9773bc396102bf3
Rajarshi Chattopadhyay <likarajo@gmail.com>
2017-11-23 17:30:34 +0530

    added shirttest.java

---
+++ src/test/java/com/example/ShirtTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class ShirtTest {
+   
+  	public static void main (String args[]) {
+ 
+	Shirt myShirt;
+	myShirt = new Shirt();
+  
+	myShirt.displayInformation();
+
+
+   	}
+}
