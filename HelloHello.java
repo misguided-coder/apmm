5b68410a8c0490af6f4a70049893fb72ab2a2a88
Rajarshi Chattopadhyay <likarajo@gmail.com>
2017-11-24 12:57:20 +0530

    added error in HelloHello.java

--- HelloHello.java
+++ HelloHello.java
@@ -1,6 +1,6 @@
 public class HelloHello{
 public static void main(String args[]){
  System.out.println("HelloHello");  
- System.out.println("123");  
+ Ssystem.out.println("123");  //error incorporated
 }
 }
