0bcc18715ef95cd99f9ebce20e002c43efeb6a16
Arindam Kotal <arikotal@in.ibm.com>
2017-11-24 12:31:31 +0530

    check user

---
+++ CheckUser.java
@@ -0,0 +1,12 @@
+public class CheckUser{
+public static void main(String args[]){
+  String user1="abc";
+  String user2="abc";
+  if(user1.equals(user2))  {
+    System.out.println("equal name");
+  }
+  else{
+    System.out.println("not equal");
+  }
+}
+}
